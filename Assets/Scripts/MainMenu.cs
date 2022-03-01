a62472f09cf545f3fbe2f14d393d03dc3fa0aa97
Anon <mail.for.feedback01@gmail.com>
2022-03-01 10:16:01 +0300

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] int TotalCash;
+    public Text AmountOfMoney;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    	TotalCash = PlayerPrefs.GetInt("TotalCash");
+    }
+
+    public void ButtonClick() // Total Cash increases
+    {
+        TotalCash++;
+        PlayerPrefs.SetInt("TotalCash", TotalCash);
+    }
+
+    public void To_Shop()
+    {
+    	SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AmountOfMoney.text = TotalCash.ToString();
+    }
+}
