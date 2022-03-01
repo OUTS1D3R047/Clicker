3515f3e376d37792e9a7392ba841425c0527cfa6
Anon <mail.for.feedback01@gmail.com>
2022-03-01 12:28:43 +0300

    Reorganisation

---
+++ Assets/Scripts/PlayScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayScene : MonoBehaviour
+{
+    [SerializeField] int TotalBugsFixed;
+    public Text AmountOfMoney;
+
+    private void Start()
+    {
+        TotalBugsFixed = PlayerPrefs.GetInt("TotalBugsFixed");
+    }
+
+    public void FixBugBtnClick()
+    {
+        TotalBugsFixed++;
+        PlayerPrefs.SetInt("TotalBugsFixed", TotalBugsFixed);
+    }
+
+    public void To_Shop()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void FixedUpdate()
+    {
+        AmountOfMoney.text = TotalBugsFixed.ToString();
+    }
+}
